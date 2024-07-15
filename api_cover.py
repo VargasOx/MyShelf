d9d4939c6a7106643371612cf4280869b2da2a68
marcelovargas <marcelovargas13@gmail.com>
2024-07-15 14:05:22 -0300

    criação script api_cover com estrtura de repetição

--- api_cover.py
+++ api_cover.py
@@ -1,13 +1,25 @@
 import requests
 import webbrowser
 
-ISBN = '978-85-3591-182-4'
+'''
+ISBN = '9788575421130'
 
 url = 'https://bookcover.longitood.com/bookcover/'+ISBN
 
 response = requests.get(url).json()['url']
 
 abrir_url = webbrowser.open(response,new = 2)
+
+'''
+
+## codigo com estrutura de repetição
+
+ISBN = ['978-8503010948']
+
+for i in ISBN:
+    url = 'https://bookcover.longitood.com/bookcover/'+i
+    response = requests.get(url).json()['url']
+    abrir_url = webbrowser.open(response,new = 2)
 
 
 
