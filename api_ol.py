53cac17894965afd7891165f8aea09e27cc4be91
marcelovargas <marcelovargas13@gmail.com>
2024-07-15 10:12:27 -0300

    criação script que busca as capas dos livros (api_cover)

--- api_ol.py
+++ api_ol.py
@@ -5,8 +5,26 @@ format = "json"
 jscmd = "details"
 ISBN = "978-85-3591-169-5"
 
-url = "http://openlibrary.org/api/books?bibkeys=ISBN:"+ISBN+"&jscmd=details&format=json"
+url_data = "http://openlibrary.org/api/books?bibkeys=ISBN:"+ISBN+"&jscmd=data&format=json"
+url_details = "http://openlibrary.org/api/books?bibkeys=ISBN:"+ISBN+"&jscmd=details&format=json"
 
-response = requests.get(url)
-print(response.json())
+def chamar_api_ol(url):
+    response = requests.get(url)
+    return response
+
+response_data = chamar_api_ol(url_data).json()
+response_details = chamar_api_ol(url_details).json()
+
+normalizado = pd.json_normalize(response_data)
+
+df_data = pd.DataFrame(normalizado)
+
+df_data.to_excel('texte.xlsx')
+
+
+
+
+
+
+
 
